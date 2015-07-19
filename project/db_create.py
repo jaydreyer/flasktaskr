25987f619289e606e15bc5caf9039343e71aa280
Jay Dreyer <jay.dreyer@gmail.com>
2015-07-18 19:32:32 -0500

    switch to SQLAlchemy for data model

--- project/db_create.py
+++ project/db_create.py
@@ -1,18 +1,8 @@
-from _config import DATABASE_PATH
-import sqlite3
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-    # get a cursor object used to execute SQL commands
-    c = connection.cursor()
-
-    # create the table
-    c.execute("""CREATE TABLE tasks(
-              task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL,
-              due_date TEXT NOT NULL,
-              priority INTEGER NOT NULL,
-              status INTEGER NOT NULL)""")
-    c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-              'VALUES("Finish this tutorial", "03/5/2015", 10, 1)')
-    c.execute('INSERT INTO  TASKS (name, due_date, priority, status)'
-              'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)')
+db.create_all()
+db.session.add(Task("Finish this tutorial",date(2015,3,13),10,1))
+db.session.add(Task("Finish Real Python",date(2015,3,13),10,1))
+db.session.commit()
